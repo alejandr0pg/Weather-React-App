a6b7fd9ddc4fc77c83c568b83d9ae889f4d47a76
Williams Perez <williams.perez@tuten.cl>
2019-10-11 14:36:19 -0400

    Add propTypes

--- src/components/WeaherTemperature.js
+++ src/components/WeaherTemperature.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+import PropTypes from 'prop-types';
+
 import { 
     CLOUD,
     CLOUDY,
@@ -32,5 +34,10 @@ const WeatherTemperature = ({ temperature, weatherState }) => (
         <span>{`${temperature} C°`}</span>
     </div>
 );
+
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired
+};
 
 export default WeatherTemperature;
