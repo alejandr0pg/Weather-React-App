37e3ebb3c2ed7877fc08207f811d7359ab4ac54a
Williams Perez <williams.perez@tuten.cl>
2019-10-11 18:27:58 -0400

    add metric units

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -9,7 +9,7 @@ const location = 'cabudare,ve';
 const api_key = '815da8056bf029e19cd6f5acf0ec07da';
 const api_url = 'http://api.openweathermap.org/data/2.5/weather';
 
-const api_weather = `${api_url}?q=${location}&appid=${api_key}`;
+const api_weather = `${api_url}?q=${location}&appid=${api_key}&units=metric`;
 
 const data = {
     temperature: 5,
