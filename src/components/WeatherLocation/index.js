4f57d234de522b6ef9ea8a90f074ec727ec3744f
Williams Perez <williams.perez@tuten.cl>
2019-10-11 18:24:36 -0400

    fetch api weather

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -5,6 +5,12 @@ import WeatherData from './WeatherData';
 import './styles.scss';
 import { SUN } from '../../constants/weathers';
 
+const location = 'cabudare,ve';
+const api_key = '815da8056bf029e19cd6f5acf0ec07da';
+const api_url = 'http://api.openweathermap.org/data/2.5/weather';
+
+const api_weather = `${api_url}?q=${location}&appid=${api_key}`;
+
 const data = {
     temperature: 5,
     weatherState: SUN,
@@ -22,12 +28,50 @@ class WeatherLocation extends Component {
         };
     }
 
+    getWeatherState = weather_data => {
+        return SUN;
+    }
+
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+
+        const data = {
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`
+        }
+
+        return data;
+    }
+
+    getCity = weather_data => {
+        const { name } = weather_data;
+
+        return name;
+    }
+
     handleUpdateClick = () => {
         console.log('actualizado');
 
-        this.setState({
-            city: 'Cabudare'
+        fetch(api_weather).then(resolve => (
+            resolve.json()
+        )).then(data => {
+            console.log(data);
+
+            const newWeather = {
+                data: this.getData(data),
+                city: this.getCity(data),
+            }
+
+            this.setState(newWeather);
         });
+
+        // this.setState({
+        //     city: 'Cabudare'
+        // });
     }
 
     render() {
